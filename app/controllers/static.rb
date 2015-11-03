60d8b6a2f3a67056c081f6b5feba4e73b5fc2bf2
NoNonsense126 <retail_nns@hotmail.com>
2015-11-03 14:27:46 +0800

    user registration and session

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,8 @@
 get '/' do
+  @user = User.find(session[:user_id]) if session[:user_id]
   erb :"static/index"
+end
+
+get '/home' do
+  redirect to '/'
 end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+	validates :email, presence: true, uniqueness: true,
+    format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\W]+\z/ }
+  validates :username, presence: true
+  validates :password, presence: true
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user && user.password == password
+      return user
+    else
+      nil
+    end
+  end
+end
